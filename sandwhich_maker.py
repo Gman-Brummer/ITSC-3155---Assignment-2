1ff5c25d686e8af2c72ac04c15142b1d2ae1230e
greyson_brummer <greyson.brummer@outlook.com>
2025-11-21 08:23:20 -0500

    Implement sandwhich maker methods

--- sandwhich_maker.py
+++ sandwhich_maker.py
@@ -1,10 +1,20 @@
+### Sandwich Maker ###
+
 class SandwichMaker:
     def __init__(self, resources):
-        self.machine_resources = resources
+        self.resources = resources
 
-    def check_resources(self, ingredients):
-        """Returns True when order can be made, False if ingredients are insufficient."""
-        #####
+    def is_resource_sufficient(self, order_ingredients):
+        """Returns True if ingredients are available, otherwise False."""
+        for item in order_ingredients:
+            if order_ingredients[item] > self.resources[item]:
+                print(f"Sorry, there is not enough {item}.")
+                return False
+        return True
 
     def make_sandwich(self, sandwich_size, order_ingredients):
-        
+        """Deduct ingredients from resources and make the sandwich."""
+        for item in order_ingredients:
+            self.resources[item] -= order_ingredients[item]
+        print(f"Here is your {sandwich_size} ham sandwich! Enjoy!")
+   
